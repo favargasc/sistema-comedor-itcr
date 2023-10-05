c331b3e579986fa4263da6e0d2bb17372570ebaf
Tania María Sánchez Irola <84118313+TaniaMariaSanchezIrola@users.noreply.github.com>
2023-10-04 18:34:00 -0600

    Revert "Cuidado que me da error"
    This reverts commit 721711e5067e64ea9fe51f4d71e176000bc0d7d4.

--- app.js
+++
@@ -1,7 +0,0 @@
-const { createUser, readUsers, updateUser, deleteUser } = require("./users");
-
-// Ejecutar las funciones según sea necesario
-createUser();
-readUsers();
-updateUser();
-deleteUser();
--- db.js
+++
@@ -1,5 +0,0 @@
-const sql = require("msnodesqlv8");
-
-const connectionString = "server=.;Database=logisticaCom;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
-
-module.exports = { sql, connectionString };
