191a6cabe3433d1e8d85a13b9dce4afe7664b4ba
Tania María Sánchez Irola <84118313+TaniaMariaSanchezIrola@users.noreply.github.com>
2023-10-09 02:51:25 -0600

    Listas las CRUD de User

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -27,8 +27,21 @@ export async function POST(req) {
   try {
     const { full_name, username, password } = req.body;  // Asumiendo que full_name, username, y password se envían en el cuerpo de la solicitud
 
+    // Verificar si el nombre de usuario ya existe
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (existingUser) {
+      // El nombre de usuario ya existe, manejar el error como desees
+      return NextResponse.error('Username already exists', 400);
+    }
+
     const encodedPassword = encodeBase64(password);
 
+    // El nombre de usuario es único, proceder con la creación del usuario
     const newUser = await prisma.user.create({
       data: {
         full_name,
@@ -46,6 +59,7 @@ export async function POST(req) {
   }
 }
 
+
 //--------------------------------------------------------------------------------------------------------------
 //  Prueba CREATE
 //--------------------------------------------------------------------------------------------------------------
@@ -56,9 +70,9 @@ async function testPostUser() {
     // Ajusta full_name, username y password según el nuevo usuario que deseas crear
     const testReq = {
       body: {
-        full_name: 'Nombre Completo 2',
-        username: 'nombreusuario2',
-        password: 'contraseña',
+        full_name: 'Nuevo Usuario',
+        username: 'nuevousuario',
+        password: 'nuevacontraseña',
       },
     };
 
@@ -69,8 +83,10 @@ async function testPostUser() {
   }
 }
 
-// Llama a la función para probar la operación POST (User) al cargar el archivo (solo para propósitos de prueba)
+
+// Llama a las funciones para probar las operaciones POST y PUT al cargar el archivo (solo para propósitos de prueba)
 //testPostUser();
+
 
 //--------------------------------------------------------------------------------------------------------------
 //  DELETE
@@ -106,7 +122,7 @@ async function testDeleteUser() {
     // Ajusta el user_id según el usuario que deseas eliminar
     const testReq = {
       body: {
-        user_id: 1,
+        user_id: 5,
       },
     };
 
@@ -124,10 +140,13 @@ async function testDeleteUser() {
 //--------------------------------------------------------------------------------------------------------------
 
 
+
 // Operación CRUD Update
 export async function PUT(req) {
   try {
     const { user_id, full_name, username, password } = req.body;  // Asumiendo que user_id, full_name, username, y password se envían en el cuerpo de la solicitud
+
+    const encodedPassword = encodeBase64(password);
 
     const updatedUser = await prisma.user.update({
       where: {
@@ -136,7 +155,7 @@ export async function PUT(req) {
       data: {
         full_name,
         username,
-        password,
+        password: encodedPassword,
       },
     });
 
@@ -152,16 +171,17 @@ export async function PUT(req) {
 //--------------------------------------------------------------------------------------------------------------
 //  Prueba UPDATE
 //--------------------------------------------------------------------------------------------------------------
+
 // Bloque de código para probar la operación PUT (solo para propósitos de prueba)
 async function testUpdateUser() {
   try {
-    // Ajusta el user_id, full_name, username y password según el usuario que deseas actualizar
+    // Ajusta user_id, full_name, username y password según el usuario que deseas actualizar
     const testReq = {
       body: {
-        user_id: 5,
-        full_name: 'Nuevo Nombre Completo',
-        username: 'nuevonombreusuario',
-        password: 'nuevacontraseña',
+        user_id: 7,
+        full_name: 'Nombre Actualizado',
+        username: 'usuarioactualizado',
+        password: 'contraseñaactualizada',
       },
     };
 
@@ -171,14 +191,38 @@ async function testUpdateUser() {
     console.error('Error en la prueba PUT (User):', error);
   }
 }
-
 // Llama a la función para probar la operación PUT (User) al cargar el archivo (solo para propósitos de prueba)
-testUpdateUser();
+//testUpdateUser();
 
 //--------------------------------------------------------------------------------------------------------------
 //  GET
 //--------------------------------------------------------------------------------------------------------------
+// Operación CRUD Get (Obtener todos los usuarios)
+export async function GET() {
+  try {
+    const users = await prisma.user.findMany();
+    return NextResponse.json(users);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    return NextResponse.error('Internal Server Error', 500);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
 
 //--------------------------------------------------------------------------------------------------------------
 //  Prueba GET
 //--------------------------------------------------------------------------------------------------------------
+// Bloque de código para probar la operación GET (solo para propósitos de prueba)
+async function testGetUsers() {
+  try {
+    const result = await GET();
+    console.log('Resultado de la prueba GET (Users):', result);
+  } catch (error) {
+    console.error('Error en la prueba GET (Users):', error);
+  }
+}
+
+// Llama a la función para probar la operación GET (Users) al cargar el archivo (solo para propósitos de prueba)
+testGetUsers();
